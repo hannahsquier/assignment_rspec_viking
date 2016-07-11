4d48f9f5b92765a616c9d706dc8a323e6cd98768
jsteenb2 <jonathansteenbergen@gmail.com>
2016-07-11 11:58:29 -0700

    added viking_spec tests

--- spec/viking_spec.rb
+++ spec/viking_spec.rb
@@ -2,7 +2,9 @@ require 'viking'
 
 describe Viking do
   let(:bob) { Viking.new("Bob", 10) }
+  let(:obo) {Viking.new("Obo", 15)}
   let(:gun) { Weapon.new("Gun") }
+  let(:sling) { Weapon.new("Sling") }
 
   describe '#initialize' do
     it "takes a name attribute" do
@@ -29,6 +31,56 @@ describe Viking do
     end
 
     it "raises an error if you pickup a non weapon" do
+      expect{bob.pick_up_weapon("not a weapon")}.to raise_error
+    end
+
+    it "pickup up weapon will replace current weapon" do
+      bob.pick_up_weapon(gun)
+      expect(bob.weapon).to eq(gun)
+      bob.pick_up_weapon(sling)
+      expect(bob.weapon).to eq(sling)
+    end
+  end
+
+  describe '#drop_weapon' do
+    it 'will drop a Viking weapon leaves the Viking weaponless' do
+      bob.pick_up_weapon(gun)
+      expect(bob.weapon).to eq(gun)
+
+      bob.drop_weapon
+      expect(bob.weapon).to eq nil
+    end
+  end
+
+  describe "#receive_attack" do
+    it 'The receive_attack method calls the take_damage method' do
+      expect(bob).to receive(:take_damage)
+
+      bob.receive_attack(10)
+    end
+
+    it 'The receive_attack method calls the take_damage method' do
+      bob.receive_attack(5)
+      expect(bob.health).to eq 5
+    end
+  end
+
+  describe '#attack' do
+    it 'does something' do
+      allow(bob).to receive(:damage_dealt).and_return(5)
+
+      bob.attack(obo)
+      expect(obo.health).to eq 10
+    end
+
+    it 'attacking another Viking calls that Viking take_damage method' do
+      expect(bob).to receive(:take_damage)
+      obo.attack(bob)
+    end
+
+    it 'attacking with no weapon runs damage_with_fists' do
+      expect(bob).to receive(:damage_with_fists)
+      bob.attack(obo)
     end
   end
 
