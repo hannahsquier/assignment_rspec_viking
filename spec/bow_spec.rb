196b379534da631144faa016de376f3e0c32c7fd
Hannah Squier <hannahsquier@gmail.com>
2016-07-11 11:35:44 -0700

    add bow specs and viking specs through pick_up_weapon

--- spec/bow_spec.rb
+++ spec/bow_spec.rb
@@ -1,1 +1,33 @@
-# Your code here
+
+require 'weapons/bow'
+
+describe Bow do
+  describe "#arrows" do 
+
+
+    it "is readable" do
+      expect{ subject.arrows }.not_to raise_error
+    end
+
+    it "initializes with 10 arrows" do
+      expect( subject.arrows ).to eq(10)
+    end
+
+    it "creates specified number of arrows" do
+      expect( Bow.new(8).arrows ).to eq(8)
+    end
+
+  end
+
+  describe "#use" do
+    it "decreases arrows by one when used" do
+      subject.use
+      expect(subject.arrows).to eq(9)
+    end
+
+    it "throws an arrow if there are 0 bows" do
+      bow_zero = Bow.new(0)
+      expect { bow_zero.use }.to raise_error
+    end
+  end
+end
--- spec/viking_spec.rb
+++ spec/viking_spec.rb
@@ -1,7 +1,35 @@
 require 'viking'
 
 describe Viking do
-  describe '#arrows' do
+  let(:bob) { Viking.new("Bob", 10) }
+  let(:gun) { Weapon.new("Gun") }
 
+  describe '#initialize' do
+    it "takes a name attribute" do
+      expect(bob.name).to eq("Bob")
+    end
+
+    it "takes a health attribute" do
+      expect(bob.health).to eq(10)
+    end
+
+    it "does not allow you to override health" do
+      expect{ bob.health = 100 }.to raise_error
+    end
+
+    it "sets viking weapon to nil initially" do
+      expect(bob.weapon).to eq(nil)
+    end
   end
+
+  describe '#pick_up_weapon' do
+    it "sets weapon to picked up weapon" do
+      bob.pick_up_weapon(gun)
+      expect(bob.weapon).to eq(gun)
+    end
+
+    it "raises an error if you pickup a non weapon" do
+    end
+  end
+
 end
